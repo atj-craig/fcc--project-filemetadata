b3138f28458f210fdabb09bba9b20d7f3017af76
Andrew Craig <atj.craig@gmail.com>
2022-07-21 21:31:48 -0600

    project complete

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config.js'
+import multer from 'multer'
+const app = express()
+const upload = multer({ dest: "./uploads/" })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file)
+  console.log(req.body)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
